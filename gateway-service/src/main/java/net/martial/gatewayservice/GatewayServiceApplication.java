e4083de678b09ce72c88d87cb02a52ef6006337d
AlainMartial <rindraniainamartial@gmail.com>
2026-03-19 17:53:15 +0300

    ROUTAGE DYNAMICS

--- gateway-service/src/main/java/net/martial/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/martial/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package net.martial.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +16,18 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
-    @Bean
+    //@Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("r1", predicates -> predicates.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
                 .route("r2", predicates -> predicates.path("/products/**").uri("lb://INVENTORY-SERVICE"))
                 .route("r3", predicates -> predicates.path("/bills/**").uri("lb://BILLING-SERVICE"))
                 .build();
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 
 }
