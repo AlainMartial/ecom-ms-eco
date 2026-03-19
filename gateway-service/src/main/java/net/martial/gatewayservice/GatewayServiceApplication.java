f3f6668a3f945efe5185e52815bcf7993b39c4ae
AlainMartial <rindraniainamartial@gmail.com>
2026-03-19 17:46:49 +0300

    ajouter "/**" dans la path

--- gateway-service/src/main/java/net/martial/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/martial/gatewayservice/GatewayServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -12,11 +13,12 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+    @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("r1", predicates -> predicates.path("/customers").uri("lb://CUSTOMER-SERVICE"))
-                .route("r2", predicates -> predicates.path("/products").uri("lb://INVENTORY-SERVICE"))
-                .route("r3", predicates -> predicates.path("/bills").uri("lb://BILLING-SERVICE"))
+                .route("r1", predicates -> predicates.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route("r2", predicates -> predicates.path("/products/**").uri("lb://INVENTORY-SERVICE"))
+                .route("r3", predicates -> predicates.path("/bills/**").uri("lb://BILLING-SERVICE"))
                 .build();
     }
 
