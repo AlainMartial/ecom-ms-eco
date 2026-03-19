a00c2638234fa6823fdee45318e2c8e8a5a776c4
AlainMartial <rindraniainamartial@gmail.com>
2026-03-19 17:44:59 +0300

    une autre configuration de routes avec la class de configuration de Java avec la @Bean Routelocator dans la class

--- gateway-service/src/main/java/net/martial/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/martial/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,22 @@ package net.martial.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("r1", predicates -> predicates.path("/customers").uri("lb://CUSTOMER-SERVICE"))
+                .route("r2", predicates -> predicates.path("/products").uri("lb://INVENTORY-SERVICE"))
+                .route("r3", predicates -> predicates.path("/bills").uri("lb://BILLING-SERVICE"))
+                .build();
     }
 
 }
